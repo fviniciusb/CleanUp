40cb2f2a97d34fff67b71caaf0204ef9fc42f3e1
Francisco Vinicius <106833771+fviniciusb@users.noreply.github.com>
2023-09-28 17:00:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function PagInicial(){
+    location.href("http://127.0.0.1:5502/index.html")
+}
+function BotaoSobre(){
+    window.open("htt://www.youtube.com","_blank")
+}
+function BotaoEntrar(){
+    window.open("htt://www.google.com","_blank")
+}
+function BotaoContato(){
+    window.open("https://www.github.com/","_blank")
+}
