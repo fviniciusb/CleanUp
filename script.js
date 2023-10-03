4dbf4f609568f71851228a1e697d8082d3c1d59e
Francisco Vinicius <106833771+fviniciusb@users.noreply.github.com>
2023-10-03 15:41:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function PagInicial(){
+var cadastro = document.querySelector("ouverlay")
+
+cadastro.addEventListener("click", function () {
+    window.open("https://www.youtube.com/", "_blank")
+});
+
+function PagInicial() {
     location.href("http://127.0.0.1:5502/index.html")
 }
-function BotaoSobre(){
-    window.open("htt://www.youtube.com","_blank")
+function BotaoSobre() {
+    window.open("https://www.youtube.com/", "_blank")
+} ps
+function BotaoEntrar() {
+    window.open("https://www.google.com", "_blank")
 }
-function BotaoEntrar(){
-    window.open("htt://www.google.com","_blank")
-}
-function BotaoContato(){
-    window.open("https://www.github.com/","_blank")
+function BotaoContato() {
+    window.open("https://www.github.com/", "_blank")
 }
